233c723e9149c978ef9c8bc6dcbd51f6fd8b6acd
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2025-11-26 22:14:45 +0530

    Refactor getters and setters in FileDB class

--- Backend_Work/File_Upload-Download_Backend/src/main/java/com/entity/FileDB.java
+++ Backend_Work/File_Upload-Download_Backend/src/main/java/com/entity/FileDB.java
@@ -29,63 +29,63 @@ public class FileDB {
   
   private String date_time;
 
-public String getDate_time() {
-	return date_time;
-}
-
-public void setDate_time(String date_time) {
-	this.date_time = date_time;
-}
-
-public String getId() {
-	return id;
-}
-
-public void setId(String id) {
-	this.id = id;
-}
-
-public String getName() {
-	return name;
-}
-
-public void setName(String name) {
-	this.name = name;
-}
-
-public String getType() {
-	return type;
-}
-
-public void setType(String type) {
-	this.type = type;
-}
-
-public byte[] getData() {
-	return data;
-}
-
-public void setData(byte[] data) {
-	this.data = data;
-}
-
-@Override
-public String toString() {
-	return "FileDB [id=" + id + ", name=" + name + ", type=" + type + ", data=" + Arrays.toString(data) + ", date_time="
-			+ date_time + "]";
-}
-
-public FileDB(String name, String type, byte[] data, String date_time) {
-	this.name = name;
-	this.type = type;
-	this.data = data;
-	this.date_time = date_time;
-}
-
-public FileDB() {
-	super();
-	// TODO Auto-generated constructor stub
-}
+	public String getDate_time() {
+		return date_time;
+	}
+	
+	public void setDate_time(String date_time) {
+		this.date_time = date_time;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public byte[] getData() {
+		return data;
+	}
+	
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "FileDB [id=" + id + ", name=" + name + ", type=" + type + ", data=" + Arrays.toString(data) + ", date_time="
+				+ date_time + "]";
+	}
+	
+	public FileDB(String name, String type, byte[] data, String date_time) {
+		this.name = name;
+		this.type = type;
+		this.data = data;
+		this.date_time = date_time;
+	}
+	
+	public FileDB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 
 }
